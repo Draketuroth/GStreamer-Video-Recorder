aa9cecca68d6b8e4bcf1933a1c2d9520c519f837
Fredrik Linde <fredrik-linde@outlook.com>
2025-03-27 23:27:32 +0100

    Added rtp stream capture test

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
 #include <gst/gst.h>
 
-// Command-line version
-// gst-launch-1.0 -e ksvideosrc ! videoconvert !  x264enc ! mp4mux ! filesink location=output.mp4
+#include <string>
+#include <iostream>
 
-// Implemented using GStreamer 1.26.0
+// The following test code is implemented using GStreamer 1.26.0
+
+enum Test {
+    None,
+    Webcam,
+    RTP
+};
 
 // Message handler for the GStreamer loop
 static gboolean bus_call(GstBus* bus,
@@ -49,9 +55,7 @@ static gboolean stop_recording(gpointer data) {
     return FALSE;
 }
 
-int main(int argc, char* argv[]) {
-    
-    // @TODO: Handle command line arguments and automize recording source, devices, format etc.
+int webcamCaptureTest() {
 
     // GStreamer elements
     GstElement* pipeline = nullptr;
@@ -66,8 +70,6 @@ int main(int argc, char* argv[]) {
     GstBus* bus = nullptr;
     GstStateChangeReturn ret;
     guint bus_watch_id;
-
-    gst_init(&argc, &argv);
 
     // Initialize all elements
     //@TODO: Look into future deprecation of kvideosrc, still supported for now. 
@@ -126,8 +128,113 @@ int main(int argc, char* argv[]) {
     g_main_loop_run(loop);
 
     gst_element_set_state(pipeline, GST_STATE_NULL);
-    gst_object_unref(GST_OBJECT(pipeline));
+    gst_object_unref(pipeline);
     g_source_remove(bus_watch_id);
     g_main_loop_unref(loop);
     g_print("Deleting pipeline\n");
+
+    return 0;
+}
+
+int rtpCaptureTest() {
+
+    // GStreamer elements
+    GstElement* pipeline = nullptr;
+
+    // GStreamer loop components
+    GMainLoop* loop = nullptr;
+    GstBus* bus = nullptr;
+    GstStateChangeReturn ret;
+    guint bus_watch_id;
+
+    // Create entire pipeline using parse_launch, this must match the sender application configuration.
+    pipeline = gst_parse_launch(
+        "udpsrc port=5000 caps=\"application/x-rtp, media=video, encoding-name=H264, payload=96\" ! "
+        "rtph264depay ! "
+        "h264parse ! "
+        "mp4mux ! "
+        "filesink location=output.mp4",
+        NULL);
+
+    if (!pipeline) {
+        g_printerr("Failed to create pipeline\n");
+        return -1;
+    }
+
+    // Create the main loop
+    loop = g_main_loop_new(NULL, FALSE);
+
+    // Get pipeline's bus (message handler)
+    bus = gst_element_get_bus(pipeline);
+    bus_watch_id = gst_bus_add_watch(bus, bus_call, loop);
+    gst_object_unref(bus);
+
+    ret = gst_element_set_state(pipeline, GST_STATE_PLAYING);
+    if (ret == GST_STATE_CHANGE_FAILURE) {
+        g_printerr("Unable to set the pipeline to the playing state. Exiting...\n");
+        gst_object_unref(pipeline);
+        return -1;
+    }
+
+    g_timeout_add_seconds(10, stop_recording, pipeline);
+
+    g_print("Begin capturing test stream...\n");
+    g_main_loop_run(loop);
+
+    gst_element_set_state(pipeline, GST_STATE_NULL);
+    gst_object_unref(pipeline);
+    g_source_remove(bus_watch_id);
+    g_main_loop_unref(loop);
+    g_print("Deleting pipeline\n");
+
+    return 0;
+}
+
+int main(int argc, char* argv[]) {
+    
+    // Initialize GStreamer
+    gst_init(&argc, &argv);
+
+    // Initialize test type
+    Test test = Test::None;
+
+    // Read input arguments, only accepting specific tests at the moment.
+    // @TODO: Handle more command line arguments and automize recording source, devices, format etc.
+    // @TODO: Add user configuration file.
+    for (int i = 1; i < argc; ++i) {
+        std::string arg = argv[i];
+
+        if (arg == "--webcam") {
+            test = Test::Webcam;
+        }
+        else if (arg == "--rtp") {
+            test = Test::RTP;
+        }
+    }
+
+    switch (test) {
+    case Test::Webcam:
+        // 1. Webcam test
+        // Requires a webcam to be plugged in (currently assuming only one camera which is the default)
+        //
+        // Configuration was based on the following command:
+        // gst-launch-1.0 -e ksvideosrc ! videoconvert !  x264enc ! mp4mux ! filesink location=output.mp4
+        webcamCaptureTest();
+        break;
+    case Test::RTP:
+        // 2. RTP capture test on local machine
+        // Requires GStreamer process running in separate terminal to simulate RTP stream using the following command:
+        // 
+        // gst-launch-1.0 -v videotestsrc is-live=true ! 
+        // x264enc tune=zerolatency bitrate=512 speed-preset=ultrafast ! 
+        // rtph264pay config-interval=1 pt=96 ! 
+        // udpsink host=127.0.0.1 port=5000
+        rtpCaptureTest();
+        break;
+    case Test::None:
+        std::cout << "GStreamer.exe <TEST>\n--webcam : Capture webcam test\n--rtp : Capture rtp stream test" << std::endl;
+        break;
+    }
+
+    return 0;
 }
